b1c4ff6daa770ce632b29392646e13c79be4788c
Ken Uehara <kenuehara@KennoMacBook-Pro.local>
2020-10-22 23:28:40 +0900

    first commit

---
+++ parity.py
@@ -0,0 +1,7 @@
+def parity(x:int) -> int:
+    result = 0
+    while x:
+        result ^= x & 1
+        x >>= 1
+    return result
+# O(n)
---
+++ point.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+// 2点 (x1,y1)と(x2,y2)tono
+kyoriwomotomeru
+kannsuu
+
+double calc_dist(double x1,double y1,double x2,double y2){
+    return sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 -y2));
+}
+
+int main(){
+    // 入力データを受け取る
+    int N;
+    cin >> N;
+    vector<double> x(N),y(N);
+    for(int i=0;i<N;++i) cin >> x[i] >> y[i];
+    double minimum_dist = 1000000000.0;
+    for(int i=0;i<N;++i){
+        for(int j=i+1;j<N;++j){
+            double dist_i_j = calc_dist(x[i],y[i],x[j],y[j]);
+            if(dist_i_j < minimum_dist){
+                minimum_dist = dist_i_j;
+            }
+        }
+    }
+    cout << minimum_dist << endl;
+}
