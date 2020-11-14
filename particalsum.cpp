7f50d18845f69e873ad32de23c6e650395bcdb8c
kuehar <kenuehara@KennoMacBook-Pro.local>
2020-11-15 03:20:09 +0900

    added recursive algorithms

---
+++ particalsum.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool func(int i,int w,const vector<int> &a){
+    // ベースケース
+    if(i==0){
+        if(w==0) return true;
+    return false;
+    }
+
+    // a[i-1]を選ばない場合
+    if(func(i-1,w,a)) return true;
+
+    // a[i-1]を選ぶ場合
+    if(func(i-1,w-a[i-1],a)) return true;
+
+    return false;
+}
+
+int main(){
+    // 入力
+    int N,W;
+    cin >> N >> W;
+    vector<int> a(N);
+    for(int i=0;i<N;++i) cin >> a[i];
+
+    if(func(N,W,a)) cout << "Yes!!" << endl;
+    else cout << "No...." << endl;
+}
