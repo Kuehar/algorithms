9ab802bd23cda81576f6b8be1c40dbdc40dd68bb
kuehar <green95251@gmail.com>
2021-02-12 23:15:34 +0900

    added buy_and_sell_stocks.py to elplain the solution of situation when we have only an opportunity to sell and buy stock.

---
+++ buy_and_sell_stocks.py
@@ -0,0 +1,10 @@
+def buy_and_sell_stock_once(prices):
+    min_price_so_far,max_profit = float('inf'),0.0
+    for price in prices:
+        max_profit_sell_today = price - min_price_so_far
+        max_profit = max(max_profit,max_profit_sell_today)
+        min_price_so_far = min(min_price_so_far,price)
+    return max_profit
+
+prices = [10,9,8,2,11,15]
+print(buy_and_sell_stock_once(prices)) # 13
