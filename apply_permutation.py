6dec702c9da832bb54477dcf2ba223408a678318
kuehar <green95251@gmail.com>
2021-02-16 21:33:06 +0900

    added introduction of apply_permutation.py

--- apply_permutation.py
+++ apply_permutation.py
@@ -1,3 +1,11 @@
+# Permutation is a reaarangement of numbers like below.
+# [1,2,3,4]
+# [1,2,4,3],[1,3,2,4],[1,3,4,2],[1,4,2,3],[1,4,3,2],
+# [2,1,3,4],[2,1,4,3],[2,3,1,4],[2,3,4,1],[2,4,1,3],[2,4,3,1],
+# ............
+# Total number of permutation about [1,2,3,4] is 24.this program calculate this number(in this case,the number is 24).
+
+# both args are list.perm is short of permutation,A is an array of n elements.
 def apply_permutation(perm,A):
     for i in range(len(A)):
         while perm[i] != i:
