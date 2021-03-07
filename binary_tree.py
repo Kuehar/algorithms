fe168056f3b15d6beffc56a5ad87e9f51327eba3
kuehar <green95251@gmail.com>
2021-03-08 00:01:43 +0900

    added method of Preorder,Inorder and Postorder to binarytree.py

--- binary_tree.py
+++ binary_tree.py
@@ -3,3 +3,14 @@ class BinaryTreeNode:
         self.data = data
         self.left = left
         self.right = right
+
+    def tree_traversal(root):
+        if root:
+            # Preorder
+            print("Preorder: %d" % root.left)
+            tree_traversal(root.left)
+
+            print("Inorder: %d" % root.data)
+            tree_traversal(root.right)
+
+            print("Postorder: %d" % root.data)
