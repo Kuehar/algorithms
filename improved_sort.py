1ea6dbd6a24a771ac65b764dd0666df2f177c04e
kuehar <green95251@gmail.com>
2021-03-28 11:45:55 +0900

    added step function to improved_sort.py

--- improved_sort.py
+++ improved_sort.py
@@ -7,12 +7,14 @@ merge two sorted arrays such as [42,72],[39,71]
 4.[42,72],[39,71] -> [39,42,71,72]
 """
 
+import random
+
 # O(N)
 def merge_arrays(left,right=[]):
     res = []
     i = j = 0
     n,m = len(left),len(right)
-    # This method will be over when it finished searching of either one.
+    # This method will be over when it finished searching either one.
     while i < n and j < m:
         if left[i] < right[j]:
             res.append(left[i])
@@ -20,5 +22,11 @@ def merge_arrays(left,right=[]):
         else:
             res.append(right[j])
             i += 1
-    # added remaining arrays element to return array.
+    # Added remaining arrays element to return array.
     return res + left[i:] + right[j:]
+
+def step(array):
+    res = []
+    for i in range(0,len(array,2):
+        res.append(merge_arrays(*array[i:i+2]))
+    return res
