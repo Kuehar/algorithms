8d29764b055871839fe85a95e636c8c86a3a2e15
kuehar <green95251@gmail.com>
2021-04-05 23:41:13 +0900

    added linked_list_reverse.py

---
+++ linked_list_reverse.py
@@ -0,0 +1,9 @@
+# Time and space complexity O(N) where n is the number of nodes in the list
+
+def reverse_linked_list(head):
+    nodes = []
+    while head:
+        nodes.append(head.data)
+        head = head.next
+    while nodes:
+        print(nodes.pop())
