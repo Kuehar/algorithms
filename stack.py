4df0590121591e91c9a2457d65e4d5547200865d
kuehar <green95251@gmail.com>
2021-04-22 08:06:04 +0900

    added explanation to  stack.py

--- stack.py
+++ stack.py
@@ -1,7 +1,14 @@
-stack = [3,4,5]
-print(stack)
-stack.append(6)
-print(stack)
-stack.append(7)
+# Stack is LIFO,Last In First Out data structure.
 
-print(stack)
+stack = [3,4,5]
+print(stack) # [3,4,5]
+stack.append(6)
+print(stack) # [3,4,5,6]
+stack.append(7)
+print(stack) # [3,4,5,6,7]
+
+stack.pop()
+print(stack) # [3,4,5,6]
+
+stack.pop()
+print(stack) # [3,4,5]
