78f5474153a801235496494d5d22b33b958126d3
kuehar <green95251@gmail.com>
2021-04-21 22:26:11 +0900

    fixed bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,11 +1,8 @@
 def bubble_sort(A,N):
-    flg = 1
     for i in range(N):
-        flg = 0
         for j in reversed(range(i+1, N)):
             if A[j] < A[j-1]:
                 A[j],A[j-1] = A[j-1],A[j]
-                flg = 1
 
 
 def main():
