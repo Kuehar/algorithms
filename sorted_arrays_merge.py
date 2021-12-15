3a0952aa36a9a3d65c4984cd4386802362539f18
Kuehar <50407759+Kuehar@users.noreply.github.com>
2021-12-15 23:57:30 +0900

    Update sorted_arrays_merge.py

--- sorted_arrays_merge.py
+++ sorted_arrays_merge.py
@@ -1,3 +1,9 @@
 def merge_sorted_arrays(sorted_arrays,):
     min_heap: List[Tuple[int,int]] = []
         sorted_arrays_iters = [iter(x) for x in sorted_arrays]
+        
+        
+        for i,it in enumerate(sorted_arrays_iters):
+            first_element = next(it,None)
+            if first_element is not None:
+                heapq.heappush(min_heap,(first_element,i))
