db0d7c8b9c2d08807488334fa32e1c52f62b85bf
kuehar <green95251@gmail.com>
2021-12-20 22:28:04 +0900

    fixed lcs.py of return value from max(dp) to max(list(map(lambda x: max(x), dp)))

--- lcs.py
+++ lcs.py
@@ -2,9 +2,9 @@ def lcs(X,Y):
     dp = [[0 for i in range(len(X)+1)] for j in range(len(Y)+1)]
     for i in range(len(Y)):
         for j in range(len(X)):
-            if X[i] ==  Y[i]:
+            if X[j] ==  Y[i]:
                 dp[i+1][j+1] = dp[i][j]+1
             else:
                 dp[i+1][j+1] = max(dp[i+1][j],dp[i][j+1])
 
-    return max(dp)
+    return max(list(map(lambda x: max(x), dp)))
